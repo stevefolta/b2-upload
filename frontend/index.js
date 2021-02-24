c2e2186f8f1b192a2fe0539e0d81bbf52f13a911
Steve Folta <steve@folta.net>
2021-02-24 15:57:58 -0800

    Frontend is working.

--- frontend/index.js
+++ frontend/index.js
@@ -1,6 +1,7 @@
 const getUploadDetailsURL = "http://localhost:3000/getUploadDetails";
 
 var uploadFileInput = null;
+var uploadFileButton = null;
 
 async function getUploadDetails() {
 	return new Promise((resolve, reject) => {
@@ -35,14 +36,80 @@ async function uploadClicked() {
 	for (const file of uploadFileInput.files) {
 		let buffer = await file.arrayBuffer();
 		let digest = await crypto.subtle.digest('SHA-1', buffer);
-		console.log(`SHA1: `, hashString(digest));
-		//*** Upload the file.
+		let hash = hashString(digest);
+		console.log(`SHA1: `, hash);
+		showFileUploadStart(file.name);
+
+		// Upload the file.
+		let xhr = new XMLHttpRequest();
+		xhr.addEventListener('load', (event) => {
+			showFileUploadDone(file.name);
+			console.info(`XHR response: ${xhr.response}`);
+			});
+		xhr.upload.addEventListener('progress', updateProgress);
+		xhr.addEventListener('error', () => {
+			showUploadError(file.name, xhr.response.status);
+			});
+		xhr.upload.addEventListener('error', () => {
+			showUploadError(file.name, xhr.response.status);
+			});
+		xhr.open("POST", uploadDetails.uploadURL);
+		xhr.setRequestHeader("Content-Type", "b2/x-auto");
+		xhr.setRequestHeader("Authorization", uploadDetails.authToken);
+		xhr.setRequestHeader("X-Bz-File-Name", encodeURIComponent(file.name));
+		xhr.setRequestHeader("X-Bz-Content-Sha1", hash);
+		xhr.send(file);
 		}
 	}
 
+function filesChanged() {
+	uploadFileButton.disabled = (uploadFileInput.files.length == 0);
+	}
+
+function showFileUploadStart(fileName) {
+	let progressBar = document.getElementById('upload-progress');
+	progressBar.hidden = false;
+	document.getElementById('which-file').hidden = false;
+	document.getElementById('uploading-file').textContent = fileName;
+	document.getElementById('error-message').hidden = true;
+	}
+
+function updateProgress(event) {
+	let progressBar = document.getElementById('upload-progress');
+	if (event.lengthComputable) {
+		let percentage = event.loaded / event.total * 100;
+		progressBar.setAttribute("value", `${percentage}`);
+		progressBar.textContent = `${percentage}%`;
+		}
+	else
+		progressBar.removeAttribute("value");
+	}
+
+function showFileUploadDone(fileName) {
+	document.getElementById('upload-progress').hidden = true;
+	document.getElementById('which-file').hidden = true;
+	let fileElement = document.createElement("div");
+	fileElement.setAttribute("class", "uploaded-file");
+	fileElement.textContent = fileName;
+	let uploadedFilesElement = document.getElementById('uploaded-files');
+	uploadedFilesElement.hidden = false;
+	uploadedFilesElement.appendChild(fileElement);
+	}
+
+function showUploadError(fileName, errorCode) {
+	document.getElementById('upload-progress').hidden = true;
+	document.getElementById('which-file').hidden = true;
+	let errorMessageElement = document.getElementById('error-message');
+	errorMessageElement.hidden = false;
+	errorMessageElement.textContent = `Error uploading "${fileName}": ${errorCode}`;
+	}
+
+
 document.addEventListener("DOMContentLoaded", () => {
 	uploadFileInput = document.getElementById('file-input');
-	const uploadFileButton = document.getElementById('upload-button');
+	uploadFileInput.addEventListener('input', filesChanged);
+	uploadFileButton = document.getElementById('upload-button');
 	uploadFileButton.addEventListener("click", uploadClicked);
 	});
+
 
